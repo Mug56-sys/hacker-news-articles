0e630934320ae2470b58feda05b5fd7447cdb9a6
Mug56-sys <kacper.bandula29@gmail.com>
2025-07-19 15:47:12 +0200

    Getting data from hackernews APi

--- hacker-news-articles/src/App.tsx
+++ hacker-news-articles/src/App.tsx
@@ -1,6 +1,15 @@
 import './index.css'
-
+import { useQuery} from "@tanstack/react-query";
 function App() {
+  const {data}=useQuery({
+    queryKey:['articles'],
+    queryFn: async()=>{
+      const response=await fetch(`https://hacker-news.firebaseio.com/v0/topstories.json`)
+      return response.json()
+    }
+  })
+  const topArticles=data?.slice(0,10);
+  console.log(topArticles)
   return (
     <div className='bg-red-50'>
       hello
