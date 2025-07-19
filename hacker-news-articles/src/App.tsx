0be22b0ba46ec067c0f530520863fa841d967406
Mug56-sys <kacper.bandula29@gmail.com>
2025-07-19 22:57:27 +0200

    final touches

--- hacker-news-articles/src/App.tsx
+++ hacker-news-articles/src/App.tsx
@@ -1,20 +1,67 @@
-import './index.css'
-import { useQuery} from "@tanstack/react-query";
-function App() {
-  const {data:articles}=useQuery({
-    queryKey:['articles'],
-    queryFn: async()=>{
-      const response=await fetch(`https://hacker-news.firebaseio.com/v0/topstories.json`)
-      return response.json()
-    }
-  })
-  const topArticles=articles?.slice(0,10);
-  console.log(topArticles)
-  return (
-    <div className='bg-red-50'>
-      hello
-    </div>
-  )
+import "./index.css";
+import { useQueries, useQuery } from "@tanstack/react-query";
+
+type Article={
+  id:number;
+  title:string;
+  by:string;
+  url:string;
+  score:number;
 }
 
-export default App
+function App() {
+  const { data, isLoading,error } = useQuery({
+    queryKey: ["articles"],
+    queryFn: async () => {
+      const response = await fetch(
+        `https://hacker-news.firebaseio.com/v0/topstories.json`
+      );
+      return response.json();
+    },
+  });
+
+  const topArticles = data?.slice(0, 10) || []
+
+  const results = useQueries<number[]>({
+        queries: topArticles.map((article:number) => ({
+            queryKey: ["article", article],
+            queryFn: async():Promise<Article> => {
+              const response = await fetch(
+                `https://hacker-news.firebaseio.com//v0/item/${article}.json`
+              );
+              return response.json();
+            },
+          })),
+      })as {
+  data: Article | undefined;
+  isLoading: boolean;
+  isError: boolean;
+}[];
+    
+  console.log(results);
+  
+
+ 
+  //console.log(FetchData.by)
+  //console.log(topArticles[1])
+  return (
+    <div className="bg-red-50">
+      {!isLoading ? (
+        results?.map((res) => {
+          
+          return <ul>
+            <li>
+            <a href={`${res?.data?.url}`} className={'text-blue-700 underline'}>{res?.data?.title}</a>
+            <p>{res?.data?.score} by {res?.data?.by}</p>
+            </li>
+            
+          </ul>
+        })
+      ) : (
+        <p>loading...</p>
+      )}
+    </div>
+  );
+}
+
+export default App;
