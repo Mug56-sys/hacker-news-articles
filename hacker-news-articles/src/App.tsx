ab576eb10a9100630fd5b2b307e1b6f8fd4ff975
Mug56-sys <kacper.bandula29@gmail.com>
2025-07-19 22:55:38 +0200

    final commit

--- hacker-news-articles/src/App.tsx
+++ hacker-news-articles/src/App.tsx
@@ -1,14 +1,14 @@
 import './index.css'
 import { useQuery} from "@tanstack/react-query";
 function App() {
-  const {data}=useQuery({
+  const {data:articles}=useQuery({
     queryKey:['articles'],
     queryFn: async()=>{
       const response=await fetch(`https://hacker-news.firebaseio.com/v0/topstories.json`)
       return response.json()
     }
   })
-  const topArticles=data?.slice(0,10);
+  const topArticles=articles?.slice(0,10);
   console.log(topArticles)
   return (
     <div className='bg-red-50'>
